6433e9611c1dab3ccd28b0b0c28741de1d7fc65b
Heavensbee <65532083+beeheavens@users.noreply.github.com>
2025-03-12 17:20:20 +0900

    [Silver V] Title: 집합, Time: 3468 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,29 +1,32 @@
 import sys
 
-m = int(sys.stdin.readline())
 S = set()
 
-for _ in range(m):
+num = int(input())
+for i in range(num):
     temp = sys.stdin.readline().strip().split()
-    
-    if len(temp) == 1:
-        if temp[0] == "all":
-            S = set([i for i in range(1, 21)])
-        else:
+    if(len(temp)==1):
+        cmd = temp[0]
+        if(cmd == 'all'):
+            S = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+        elif(cmd == 'empty'):
             S = set()
     
     else:
-        func, x = temp[0], temp[1]
-        x = int(x)
-
-        if func == "add":
-            S.add(x)
-        elif func == "remove":
-            S.discard(x)
-        elif func == "check":
-            print(1 if x in S else 0)
-        elif func == "toggle":
-            if x in S:
-                S.discard(x)
+        cmd , n = temp[0],temp[1]
+        n = int(n)
+        if(cmd == 'add'):
+            S.add(n)
+        elif(cmd == 'remove'):
+            S.discard(n)
+        elif(cmd == 'check'):
+            if(n in S):
+                print(1)
             else:
-                S.add(x)
+                print(0)
+        elif(cmd == 'toggle'):
+            if(n in S):
+                S.discard(n)
+            else:
+                S.add(n)
+    
