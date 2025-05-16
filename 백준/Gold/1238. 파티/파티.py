45122b9554f68908a668f1f5078dcee533c35d6b
Heavensbee <65532083+beeheavens@users.noreply.github.com>
2025-05-16 13:21:22 +0900

    [Gold III] Title: 파티, Time: 1788 ms, Memory: 35508 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,41 @@
+import heapq
+N, M, X = map(int, input().split())
+
+# 인접 리스트로 변경
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))  # (도착노드, 비용)
+            
+def dijkstra(start, end):
+    distance = [float('inf')] * (N+1)
+    distance[start] = 0
+    queue = [(0, start)]
+    
+    while queue:
+        dist, now = heapq.heappop(queue)
+        if distance[now] < dist:
+            continue
+        
+        for next_node, cost in graph[now]:
+            new_cost = dist + cost
+            if new_cost < distance[next_node]:
+                distance[next_node] = new_cost
+                heapq.heappush(queue, (new_cost, next_node))
+                
+    return distance[end]
+
+go = []
+for i in range(1, N+1):
+    go.append(dijkstra(i, X))
+    
+back = []
+for i in range(1, N+1):
+    back.append(dijkstra(X, i))
+
+ans = []
+for i in range(N):
+    ans.append(go[i] + back[i])
+
+print(max(ans))
